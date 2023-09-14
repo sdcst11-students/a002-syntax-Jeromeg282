da5d85556edfeb6d602ffa137f5b95ac4ee7df25
Jeromeg282 <113056411+Jeromeg282@users.noreply.github.com>
2023-09-14 13:19:19 -0700

    .

--- assignment.py
+++ assignment.py
@@ -1,8 +1,23 @@
- "help", "copyright", "credits" or "license" for more information.
->>> import turtle
->>> s = turtle.getscreen()
->>> t= turtle.Turtle
->>> t.fd(200)
->>> t.forward(50)
->>> t.lt(30)
->>> t.forward(35)
+import turtle
+s = turtle.getscreen()
+t= turtle.Turtle
+turtle.title("Turtle City")
+turtle.bgcolor("black")
+
+def building():
+    turtle.goto(250, -250)
+    turtle.fillcolor("gray")
+    t.begin_fill()
+    t.setheading(90)
+    t.fd(100)
+    t.setheading(90)
+    t.fd(100)
+    
+
+
+
+
+
+
+
+i = input()
---
+++ sample.py
@@ -0,0 +1,9 @@
+import turtle
+s = turtle.getscreen()
+t= turtle.Turtle()
+t.fd(200)
+t.forward(50)
+t.lt(30)
+t.forward(35)
+for i in range(300):
+    t.left(90)
